a6c5de332b132d90c8c371f68e146eedc10f706d
Subash <subashsuni55@gmail.com>
2026-03-03 18:25:02 +0530

    feat: Add a search input and button within a card to the hero section.

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,8 +1,12 @@
+import { Button } from "../ui/button";
+import { Card, CardContent } from "../ui/card";
+import { Input } from "../ui/input";
+
 const Hero = () => {
   return (
     <section className="bg-[url('/assets/img/library.jpeg')] bg-cover bg-center bg-no-repeat min-h-dvh">
       <div className="bg-black/30 min-h-dvh relative">
-        <div className="container mx-auto px-4 py-16 ">
+        <div className="container mx-auto px-4 py-16 flex flex-col items-center justify-center gap-8">
           <h1 className="text-7xl font-bold text-white text-center">
             <span>Looking for a </span>
             <br />
@@ -10,8 +14,26 @@ const Hero = () => {
               particular book?
             </span>
           </h1>
+          <Card className="w-full max-w-md bg-gray-300 z-10">
+            <CardContent>
+              <form action="">
+                <div className="flex flex-col gap-4">
+                  <Input
+                    type="text"
+                    placeholder="Search for books,authors,library here..."
+                  />
+                  <Button
+                    type="submit"
+                    className="bg-blue-500 rounded-3xl font-bold"
+                  >
+                    Search
+                  </Button>
+                </div>
+              </form>
+            </CardContent>
+          </Card>
         </div>
-        <div className="absolute bottom-0 left-0 right-0 flex items-baseline justify-between">
+        <div className="absolute bottom-0 left-0 right-0 flex items-baseline justify-between ">
           <img
             src="/assets/img/person1.png"
             alt=""
